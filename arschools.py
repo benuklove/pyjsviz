cb57125bd5f355bc9d7121d0acfd467f18f8992c
Ben Love <benuklove@users.noreply.github.com>
2016-09-08 14:26:29 -0500

    Arkansas ACT score mapping file added
    Will update to display other variables but for now just average
    composite ACT scores per high school.

--- arschools.py
+++ arschools.py
@@ -9,7 +9,10 @@
 """
 
 import pandas as pd
+import numpy as np
+
 from bokeh.io import output_file, show
+from bokeh.palettes import RdYlGn10 as palette
 from bokeh.models import (
     GMapPlot, GMapOptions, ColumnDataSource, Circle, DataRange1d, PanTool,
     WheelZoomTool, HoverTool
@@ -21,12 +24,15 @@ plot = GMapPlot(
     api_key='AIzaSyA8KPoava_5gEOoChBlr2yOEC0vNxrTQ_Y',
     x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options
 )
-plot.title.text = "Arkansas Schools"
 
+plot.title.text = "Arkansas Schools' Average ACT Scores"
+
+# Get cleaned data after wrangling
 df = pd.read_csv("data/arschools.csv")
 lea = df['LEA'].tolist()
 avcomp = df['AvgComposite'].tolist()
 latlngs = df['latlongs'].tolist()
+
 # Convert latlng strings to individual lats & lngs as floats in separate lists
 latlonglist = []
 for ll in latlngs:
@@ -38,27 +44,68 @@ for tupl in latlonglist:
     latitudes.append(float(tupl[0]))
     longitudes.append(float(tupl[2]))
 
-# print(lea)
-# print(avcomp)
+# Color mapping (there must be a better way)
+palette.reverse()
+colors = []
+for act in avcomp:
+    if act <= 16:
+        colors.append(palette[0])
+    elif 16 < act <= 17:
+        colors.append(palette[1])
+    elif 17 < act <= 18:
+        colors.append(palette[2])
+    elif 18 < act <= 19:
+        colors.append(palette[3])
+    elif 19 < act <= 20:
+        colors.append(palette[4])
+    elif 20 < act <= 21:
+        colors.append(palette[5])
+    elif 21 < act <= 22:
+        colors.append(palette[6])
+    elif 22 < act <= 24:
+        colors.append(palette[7])
+    elif 24 < act <= 26:
+        colors.append(palette[8])
+    elif act > 26:
+        colors.append(palette[9])
+    else:
+        colors.append("#F3F1ED")
 
-# source = ColumnDataSource(
-#     data=dict(
-#         lat=[36.29, 34.20, 35.29],
-#         lon=[-94.30, -92.74, -90.78],
-#     )
-# )
+# Minimize circle size of schools not taking ACT (primaries, etc)
+size = []
+for value in avcomp:
+    if np.isnan(value):
+        size.append(1)
+    else:
+        size.append(15)
 
+# Data to be visualized (equal length arrays)
 source = ColumnDataSource(
     data=dict(
         lat=latitudes,
         lon=longitudes,
+        l=lea,
+        act=avcomp,
+        c=colors,
+        s=size,
     )
 )
 
-circle = Circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8,
-                line_color=None)
-plot.add_glyph(source, circle)
+# Circle glyph with relevant properties for plotting
+circle = Circle(x="lon", y="lat", size="s", fill_color="c",
+                fill_alpha=0.8, line_color=None)
 
-plot.add_tools(PanTool(), WheelZoomTool(), HoverTool())
+# Information to be displayed when hovering over glyphs
+hover = HoverTool(
+    tooltips=[
+        ("(lat,lng)", "(@lat, @lon)"),
+        ("LEA", "@l"),
+        ("Average Composite ACT", "@act"),
+    ]
+)
+
+# Render the plot
+plot.add_glyph(source, circle)
+plot.add_tools(PanTool(), WheelZoomTool(), hover)
 output_file("gmap_plot.html")
 show(plot)
